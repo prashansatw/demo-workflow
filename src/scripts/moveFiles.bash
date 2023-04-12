ae4820e4c54aff99a67667fbf7dd8884d7951c92
prashansasaxena <prashansasaxena16@gmail.com>
2023-04-12 17:46:29 +0530

    [Prashansa/Ruchith] Added moveFiles.bash

---
+++ src/scripts/moveFiles.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+pushd ../pg-website/registry && \
+    mkdir static && \
+    cp -a ../../pg-scripts/packages/registry/build/static/* static && \
+    cp ../../pg-scripts/packages/registry/build/index.html . && \
+popd
+
+pushd ../pg-website/eligibility && \
+    mkdir static && \
+    cp -a ../../pg-scripts/packages/eligibility/build/static/* static && \
+    cp ../../pg-scripts/packages/eligibility/build/index.html . && \
+popd
+
+# pushd ../pg-website/map && \
+#     git rm -rf _next/* || true && \
+#     mkdir _next && \
+#     cp -a ../../pg-scripts/packages/map/build/_next/static _next && \
+#     cp -a ../../pg-scripts/packages/map/build/_next/data _next && \
+#     git add _next/* && \
+#     cp ../../pg-scripts/packages/map/build/index.html . && \
+# popd
+
+pushd ../pg-website/roadmap && \
+    mkdir static && \
+    cp -a ../../pg-scripts/packages/roadmap/build/static/* static && \
+    cp ../../pg-scripts/packages/roadmap/build/index.html . && \
+popd
+
+pushd ../pg-website/community && \
+    cp -a ../../pg-scripts/packages/community/* . && \
+    
+popd
