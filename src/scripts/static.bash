6ce6b34416a2e441e2cbdfbd34fe728f23dd25ba
prashansasaxena <prashansasaxena16@gmail.com>
2023-04-21 13:16:25 +0530

    [Prashansa] changing urls in static.bash file

--- src/scripts/static.bash
+++ src/scripts/static.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOMAIN="https://prashansatw.github.io/"
+DOMAIN="https://prashansatw.github.io/demo-workflow-test/"
 
 # source a static version of the site
 wget --no-host-directories --recursive --page-requisites --no-parent --timestamping -e robots=off -w 1 --random-wait --retry-on-http-error=503 -P static http://dpgwebsite.herokuapp.com http://dpgwebsite.herokuapp.com/submission-guide/
@@ -19,19 +19,19 @@ done
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo 'Detected OSX...'
-	find . -name '*.html' -exec sed -i '' -e 's_http://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_https://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
-	find . -name '*.html' -exec sed -i '' -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_http://dpgwebsite.herokuapp.com/__g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_https://dpgwebsite.herokuapp.com/__g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/__g' {} \;
+	find . -name '*.html' -exec sed -i '' -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/__g' {} \;
 	# After all URLs with page links after the / have been replaced,
 	# anything without trailing / is replaced with the actual domain
 	find . -name '*.html' -exec sed -i '' -e "s,http://dpgwebsite.herokuapp.com,$DOMAIN,g" {} \;
 else
 	echo 'Detected non-OSX...'
-	find . -name '*.html' -exec sed -i -e 's_http://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_https://dpgwebsite.herokuapp.com/_/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
-	find . -name '*.html' -exec sed -i -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/_\\/_g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_http://dpgwebsite.herokuapp.com/__g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_https://dpgwebsite.herokuapp.com/__g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_http:\\/\\/dpgwebsite.herokuapp.com\\/__g' {} \;
+	find . -name '*.html' -exec sed -i -e 's_https:\\/\\/dpgwebsite.herokuapp.com\\/__g' {} \;
 	# After all URLs with page links after the / have been replaced,
 	# anything without trailing / is replaced with the actual domain
 	find . -name '*.html' -exec sed -i -e "s,http://dpgwebsite.herokuapp.com,$DOMAIN,g" {} \;
